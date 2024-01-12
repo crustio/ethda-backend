e2f8daaba35b118da07ec4cec00cd585175da124
KayleCoder <kalecoder@outlook.com>
2024-01-12 14:46:54 +0800

    Base code for backend
    Signed-off-by: KayleCoder <kalecoder@outlook.com>

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ethda-backend
+
+## Configuration
+
+ethda-backend is configured using environment variables. You can define them using export or with an .env file.
+
+We provide a .env.example file as reference which you can use as starting point.
+
+```sell
+cp .env.example .env
+```
+
+## Running ethda-backend locally
+
+Install dependencies
+
+```sell
+npm install
+```
+
+Start server
+
+```sell
+npm run dev
+```
+
+
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+import { resolve } from 'path';
+import {commitmentsToVersionedHashes, EncodeBlobs} from '@ethda/blobs';
+import {blobToKzgCommitment, computeBlobKzgProof, loadTrustedSetup} from "c-kzg";
+const app = express();
+const port = process.env.PORT ?? 3000;
+const SETUP_FILE_PATH = resolve(__dirname, 'trusted.txt');
+loadTrustedSetup(SETUP_FILE_PATH);
+app.use(cors({
+    origin:true,
+    credentials: true
+}));
+app.use(bodyParser.json());
+app.post('/convert/blob', (req: any, res: any) => {
+    const content = req.body.content;
+    if (content) {
+        const blobs = EncodeBlobs(Buffer.from(content, 'utf-8'));
+        const commitments = [];
+        const proofs = [];
+        const versionedHashes = [];
+        for (let i = 0; i < blobs.length; i++) {
+            commitments.push(blobToKzgCommitment(blobs[i]));
+            proofs.push(computeBlobKzgProof(blobs[i], commitments[i]));
+            versionedHashes.push(commitmentsToVersionedHashes(commitments[i]));
+        }
+        res.json({
+            commitments,
+            proofs,
+            versionedHashes
+        });
+    } else {
+        res.status(400).send('content undefined');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`server start at: ${port}`);
+});
